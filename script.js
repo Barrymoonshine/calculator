35f7053234f1e02bf7855fed7dc09528fab122ec
Nick Sheward <nickshwrd@gmail.com>
2023-01-03 16:01:09 +0000

    Created functions for operators and added event listeners

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-const operatorBtns = document.getElementById('opBtns');
+const operatorBtns = document.getElementsByClassName('opBtns');
+
+const getUserBtnChoice = function (e) {
+    applyUserBtnChoice(e.target.id);
+}
+
+for (i of operatorBtns) {
+    i.addEventListener('click', getUserBtnChoice);
+}
+
+function applyUserBtnChoice(userChoice) {
+    if (userChoice == 'add') {
+        add();
+    } else if (userChoice == 'subtract') {
+        subtract();
+    } else if (userChoice == 'multiply') {
+        multiply();
+    } else if (userChoice == 'divide') {
+        divide();
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
