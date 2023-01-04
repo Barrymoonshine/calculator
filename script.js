1b07e917111faebdb1c5213ae8e0bb7b44daaefa
Nick Sheward <nickshwrd@gmail.com>
2023-01-04 10:43:16 +0000

    Created number buttons for the calculator and operate function

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
 const operatorBtns = document.getElementsByClassName('opBtns');
-const container = document.getElementById('container');
-const strTwo = document.createElement('div');
-strTwo.id = 'userOutput';
-container.appendChild(strTwo);
+const numberBtns = document.getElementsByClassName('numBtns');
+const displayContainer = document.getElementById('displayContainer');
+const displayOutput = document.createElement('div');
+displayOutput.id = 'displayOutput';
+displayContainer.appendChild(displayOutput);
+
 
 const getUserBtnChoice = function (e) {
     applyUserBtnChoice(e.target.id);
 }
 
+const getUserNumButton = function (e) {
+    applyUserNumButton(e.target.id);
+}
+
+for (i of numberBtns) {
+    i.addEventListener('click', getUserNumButton);
+}
+
+function applyUserNumButton(num) {
+    displayOutput.textContent = `${num}`;
+}
+
+
 for (i of operatorBtns) {
     i.addEventListener('click', getUserBtnChoice);
 }
 
-function applyUserBtnChoice(userChoice) {
+function operate(a, b, userChoice) {
     if (userChoice == 'add') {
-        strOne = document.getElementById('userInput').value;
-        add(strOne);
-        console.log(userChoice);
-        // } else if (userChoice == 'subtract') {
-        //     operate();
-        // } else if (userChoice == 'multiply') {
-        //     operate(*);
-        // } else if (userChoice == 'divide') {
-        //     operate(/);
-        // }
+        add(a, b);
+    } else if (userChoice == 'subtract') {
+        subtract(a, b);
+    } else if (userChoice == 'multiply') {
+        multiply(a, b);
+    } else if (userChoice == 'divide') {
+        divide(a, b);
     }
 }
 
-
-
-function add(strOne) {
-    console.log(strOne);
-    strTwo.textContent = `${strOne} +`;
+function add(a, b) {
+    return a + b;
 }
 
-// function subtract(a, b) {
-//     return a - b;
-// }
+function subtract(a, b) {
+    return a - b;
+}
 
-// function multiply(a, b) {
-//     return a * b;
-// }
+function multiply(a, b) {
+    return a * b;
+}
 
-// function divide(a, b) {
-//     return a / b;
-// }
+function divide(a, b) {
+    if (b == 0) {
+        alert('math ERROR');
+    } else {
+        return a / b;
+    }
+}
 
