38f091bdde34c76502959a973e540f254246d858
Nick Sheward <nickshwrd@gmail.com>
2023-01-04 14:53:44 +0000

    Updated add function so multiple numbers can now be added together

--- script.js
+++ script.js
@@ -22,34 +22,33 @@ for (i of numberBtns) {
 function applyUserNumButton(num) {
     displayInput.textContent += num;
     displayInputValue = document.getElementById('displayInput').textContent;
-    console.log(displayInputValue);
 }
 
 function operate(userChoice) {
     if (userChoice == 'add') {
         add();
-        console.log(userChoice);
     } else if (userChoice == 'subtract') {
         subtract(a, b);
-        console.log(userChoice);
     } else if (userChoice == 'multiply') {
         multiply(a, b);
-        console.log(userChoice);
     } else if (userChoice == 'divide') {
         divide(a, b);
-        console.log(userChoice);
     } else if (userChoice == 'equals') {
         equals();
-        console.log(userChoice);
     } else if (userChoice == 'clear') {
         clear();
-        console.log(userChoice);
     }
 }
 
 function add() {
-    displayOutput.textContent = ` ${displayInputValue} +`
-    displayInput.textContent = '';
+    if (typeof result === 'undefined') {
+        displayOutput.textContent = ` ${displayInputValue} +`
+        displayInput.textContent = '';
+    } else {
+        displayOutput.textContent = ` ${result} +`
+        displayInput.textContent = ``;
+        console.log(result);
+    }
 }
 
 function subtract(a, b) {
@@ -72,7 +71,7 @@ function equals() {
     displayOutputValue = document.getElementById('displayOutput').textContent;
     let calc = `${displayOutputValue}${displayInputValue} =`;
     displayOutputValue.replace('+', '');
-    let result = (parseInt(displayInputValue, 10)) + (parseInt(displayOutputValue, 10));
+    result = (parseInt(displayInputValue, 10)) + (parseInt(displayOutputValue, 10));
     displayOutput.textContent = calc;
     displayInput.textContent = result;
     console.log(result)
