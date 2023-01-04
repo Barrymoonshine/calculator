7889a36941d01cc9a93388b090fea33b2183deb9
Nick Sheward <nickshwrd@gmail.com>
2023-01-04 13:54:49 +0000

    Created clear function and create display function

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
 const operatorBtns = document.getElementsByClassName('opBtns');
 const numberBtns = document.getElementsByClassName('numBtns');
 const displayContainer = document.getElementById('displayContainer');
-const displayOutput = document.createElement('div');
-displayOutput.id = 'displayOutput';
-displayContainer.appendChild(displayOutput);
 
+document.body.onload = createDisplay();
 
 const getUserBtnChoice = function (e) {
-    applyUserBtnChoice(e.target.id);
+    operate(e.target.id);
 }
 
 const getUserNumButton = function (e) {
     applyUserNumButton(e.target.id);
 }
 
+for (i of operatorBtns) {
+    i.addEventListener('click', getUserBtnChoice);
+}
 for (i of numberBtns) {
     i.addEventListener('click', getUserNumButton);
 }
 
 function applyUserNumButton(num) {
-    displayOutput.textContent = `${num}`;
+    displayInput.textContent += num;
+    displayInputValue = document.getElementById('displayInput').textContent;
+    console.log(displayInputValue);
 }
 
-
-for (i of operatorBtns) {
-    i.addEventListener('click', getUserBtnChoice);
-}
-
-function operate(a, b, userChoice) {
+function operate(userChoice) {
     if (userChoice == 'add') {
-        add(a, b);
+        add();
+        console.log(userChoice);
     } else if (userChoice == 'subtract') {
         subtract(a, b);
     } else if (userChoice == 'multiply') {
         multiply(a, b);
     } else if (userChoice == 'divide') {
         divide(a, b);
+    } else if (userChoice == 'clear') {
+        clear();
+        console.log(userChoice);
     }
 }
 
-function add(a, b) {
-    return a + b;
+function add() {
+    displayOutput.textContent = ` ${displayInputValue} +`
+    let displayOutputValue = document.getElementById('displayOutput').textContent;
+    displayOutputValue.replace('+', '');
+    displayInputValue = Number(displayInputValue) + Number(displayOutputValue);
 }
 
 function subtract(a, b) {
@@ -57,5 +62,21 @@ function divide(a, b) {
     } else {
         return a / b;
     }
+}
+
+function clear() {
+    while (displayContainer.lastElementChild) {
+        displayContainer.removeChild(displayContainer.lastElementChild);
+    }
+    createDisplay();
+}
+
+function createDisplay() {
+    const displayInput = document.createElement('div');
+    displayInput.id = 'displayInput';
+    displayContainer.appendChild(displayInput);
+    const displayOutput = document.createElement('div');
+    displayOutput.id = 'displayOutput';
+    displayContainer.appendChild(displayOutput);
 }
 
